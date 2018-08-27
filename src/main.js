30366f2efa58eb11a919a84e4f667fbca0699aca
扶俊冰 <583586541@qq.com>
2018-08-27 08:58:46 +0800

    213121231

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router/router'
+import router from './config/router'
 
 import Navigation from 'vue-navigation'
 Vue.use(Navigation, {router})
