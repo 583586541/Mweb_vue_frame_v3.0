93a36e8a22ce4f634a1df40ce833c80fe015c81b
扶俊冰 <583586541@qq.com>
2018-09-21 17:34:18 +0800

    11111

--- src/views/downApp.vue
+++ src/views/downApp.vue
@@ -6,9 +6,12 @@
       <p>下载牛掰旅游，开启你牛掰的人生</p>
       <img src="../assets/images/logo.png" alt="logo">
       <div class="url">
-        <a href="https://www.pgyer.com/niubailvyou">点击下载</a>
+        <a :href="downSrc">点击下载</a>
       </div>
     </div>
+    <div class="guide" v-if="$.isWeixinClient()">
+      <img src="../assets/images/guide_down.png" alt="请前往浏览器下载">
+    </div> 
   </div>
 </template>
 
@@ -20,7 +23,17 @@
       return {
         FHeaderCfg: {
           title: '下载App'
-        }
+        },
+        downSrc: ''
+      }
+    },
+    created() {
+      let _this = this
+
+      if (_this.$.getPhoneSystem() == 'iPhone') {
+        _this.downSrc = 'http://oss.gznbly.com/ios/niub.ipa'
+      } else {
+        _this.downSrc = 'http://oss.gznbly.com/android/niub.apk'
       }
     },
     components: {
